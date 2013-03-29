29ea3f122558390f0bf61930bc5b0eddecdb6a37
Gareth Rushgrove <gareth@morethanseven.net>
2013-03-29 20:24:39 +0000

    move config files into config away from service

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,11 +2,21 @@ class riemann::config {
 
   $host = $riemann::host
   $port = $riemann::port
+  $config_file = $riemann::config_file
+
+  file { '/etc/init.d/riemann':
+    ensure => link,
+    target => '/lib/init/upstart-job',
+  }
+
+  file { '/etc/init/riemann.conf':
+    ensure  => present,
+    content => template('riemann/init/riemann.conf.erb')
+  }
 
   file { '/etc/riemann.sample.config':
     ensure => present,
     source => 'puppet:///modules/riemann/riemann.config',
-    notify => Service['riemann']
   }
 
   file { '/etc/puppet/riemann.yaml':
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,27 +1,9 @@
 class riemann::service {
-  $config_file = $riemann::config_file
-
-  file { '/etc/init.d/riemann':
-    ensure => link,
-    target => '/lib/init/upstart-job',
-  }
-
-  file { '/etc/init/riemann.conf':
-    ensure  => present,
-    content => template('riemann/init/riemann.conf.erb')
-  }
-
   service {'riemann':
     ensure     => running,
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
     provider   => upstart,
-    require    => [
-      File['/etc/init.d/riemann'],
-      File['/etc/init/riemann.conf'],
-    ],
   }
-
-  File['/etc/init/riemann.conf'] ~> Service['riemann']
 }
