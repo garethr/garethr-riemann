034799d6347575f51b4f58ba1edab4c1d1b1567c
Gareth Rushgrove <gareth@morethanseven.net>
2013-03-29 20:26:31 +0000

    tidy up config for dash

--- manifests/dash/config.pp
+++ manifests/dash/config.pp
@@ -1,6 +1,17 @@
 class riemann::dash::config {
   $host = $riemann::dash::host
   $port = $riemann::dash::port
+
+  file { '/etc/init.d/riemann-dash':
+    ensure => link,
+    target => '/lib/init/upstart-job',
+  }
+
+  file { '/etc/init/riemann-dash.conf':
+    ensure  => present,
+    content => template('riemann/riemann-dash.conf.erb'),
+  }
+
   file { '/etc/riemann-dash.rb':
     ensure => present,
     content => template('riemann/etc/riemann-dash.rb.erb'),
--- manifests/dash/service.pp
+++ manifests/dash/service.pp
@@ -1,21 +1,9 @@
 class riemann::dash::service {
-  file { '/etc/init.d/riemann-dash':
-    ensure => link,
-    target => '/lib/init/upstart-job',
-  }
-
-  file { '/etc/init/riemann-dash.conf':
-    ensure  => present,
-    content => template('riemann/riemann-dash.conf.erb'),
-    notify  => Service['riemann-dash'],
-  }
-
   service {'riemann-dash':
     ensure     => running,
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
     provider   => upstart,
-    require    => File['/etc/init.d/riemann-dash'],
   }
 }
