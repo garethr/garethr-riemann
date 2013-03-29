2ed20f171b9394cde746e516e52bbbae34129235
Gareth Rushgrove <gareth@morethanseven.net>
2013-03-29 21:29:34 +0000

    add validation to dash and tools classes

--- manifests/dash.pp
+++ manifests/dash.pp
@@ -2,6 +2,7 @@ class riemann::dash(
   $host = $riemann::params::dash_host,
   $port = $riemann::params::dash_port,
 ){
+  validate_string($host)
   class { 'riemann::dash::install': } ->
   class { 'riemann::dash::config': } ~>
   class { 'riemann::dash::service': } ->
--- manifests/tools.pp
+++ manifests/tools.pp
@@ -1,7 +1,8 @@
 class riemann::tools(
-  $health_enabled=true,
-  $net_enabled=true,
+  $health_enabled = true,
+  $net_enabled = true,
 ){
+  validate_bool($health_enabled, $net_enabled)
   class { 'riemann::tools::install': } ->
   class { 'riemann::tools::config': } ~>
   class { 'riemann::tools::service': } ->
